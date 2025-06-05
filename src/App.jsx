83253eb3b9458d93df024b92726834efc4faf383
ranako <rshivam993909@gmail.com>
2025-06-05 14:22:40 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import './App.css'
 import Hero from './hero/hero.jsx'
 import Navbar from './navbar/navbar.jsx'
-import OurTeams from './ourteams/ourteams.jsx'
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <OurTeams />
 
     </>
   )
